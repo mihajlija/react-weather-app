75570fb2339b4093eb79fd3d441a7fbfabbc054c
Milica <mihajlija@gmail.com>
2018-08-01 15:58:13 +0200

    update README

--- src/App.js
+++ src/App.js
@@ -173,7 +173,9 @@ class Error extends Component {
   render () {
     return (
       <div className='city'>
-        <p className='error'>{this.props.error}</p>
+        <div className='mid'>
+          <p className='error'>{this.props.error}</p>
+        </div>
         <div className='close' onClick={this.props.discard}>x</div>
       </div>
     )
